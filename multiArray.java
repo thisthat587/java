33adfa1b3fff5d25b57bc000fac1044e4d2775c6
thisthat587 <thisthat587@gmail.com>
2023-11-25 12:52:56 +0530

    2d array

--- multiArray.java
+++ multiArray.java
@@ -1,9 +1,11 @@
 public class multiArray {
     public static void main(String[] args) {
-        int arr[][] = { { 2, 3 }, { 3, 4 }, { 4, 5 } };
-        for (int i=0;i<3;i++){
-            
-            System.out.println(arr[][]);
+        int arr[][] = { { 2, 3 }, { 3, 4, 4, 6 }, { 4, 5, 1} };
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
         }
     }
 }
