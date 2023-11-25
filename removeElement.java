c7ce634ec5451c407d700e5d23724c4b07049eec
thisthat587 <thisthat587@gmail.com>
2023-11-25 14:27:21 +0530

    remove element program

--- removeElement.java
+++ removeElement.java
@@ -1,13 +1,16 @@
 import java.util.Arrays;
+
 public class removeElement {
     public static void main(String[] args) {
-        int arr[]={2,3,4,5,6,7,8,9};
-        int index=2;
-        for(int i=arr.length-1;i>index;i--){
-            arr[i-1]=arr[i];
+        int arr[] = { 2, 3, 4, 5, 6, 7, 8, 9 };
+        String array = Arrays.toString(arr);
+        System.out.println(array);
+        int index = 2;
+        for (int i = index; i < arr.length - 1; i++) {
+            arr[i] = arr[i + 1];
         }
-        arr[arr.length-1]=0;
-        String array=Arrays.toString(arr);
+        arr[arr.length - 1] = 0;
+        array = Arrays.toString(arr);
         System.out.println(array);
     }
 }
