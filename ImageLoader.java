67d3cf532a0e2568ac88faa65d8a48bac978fd6a
thisthat587 <thisthat587@gmail.com>
2023-12-29 20:22:58 +0530

    .

--- ImageLoader.java
+++ ImageLoader.java
@@ -1,48 +1,33 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class ImageLoader extends JFrame {
-
-    ImageLoader() {
-        this.add(new ImagePanel());
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(1280, 797);
-        setVisible(true);
-    }
-
-    public static void main(String[] args) {
-        new ImageLoader();
-    }
-}
-
 class ImagePanel extends JPanel {
-    private Image dot;
+    private Image tree;
 
     public ImagePanel() {
-        loadImage();
-        setPreferredSize(new Dimension(1280, 797));
-    }
-
-    private void loadImage() {
-        try {
-            System.out.println("Loading Image...");
-            ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("resources\\tree.png"));
-            dot = i1.getImage();
-            if (dot == null) {
-                throw new RuntimeException("Image not found");
-            }
-            repaint(); // Repaint the panel after loading the image
-        } catch (Exception e) {
-            System.err.println("Error loading image: " + e.getMessage());
-        }
+        ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("resources\\tree.png"));
+        tree=i1.getImage();
+        setPreferredSize(new Dimension(1280,797));
     }
 
     @Override
-    protected void paintComponent(Graphics g) {
+    public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        if (dot != null) {
-            System.out.println("Drawing Image"); // Debugging print
-            g.drawImage(dot, 0, 0, this);
-        }
+        g.drawImage(tree,0,0,this);
+    }
+}
+
+class ImageLoader extends JFrame {
+
+    public ImageLoader() {
+        this.add(new ImagePanel());
+        setSize(1280,797);
+        setVisible(true);
+        setResizable(false);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public static void main(String args[]) {
+        new ImageLoader();
     }
 }
